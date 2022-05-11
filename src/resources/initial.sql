59b5d616899950410b857854f88488b8c095bf35
aurojitbasak <ext-aurojit.basak@elisa.fi>
2022-05-11 16:38:22 +0530

    initial.sql updated

--- src/resources/initial.sql
+++ src/resources/initial.sql
@@ -1,34 +1,40 @@
 create table test_order (
     order_id varchar(10) primary key,
-    quantity int(5),
-    type varchar(10)
+    quantity int(5) not null,
+    type varchar(10) not null
 );
 
 create table test_order_status (
-   order_id varchar(10),
-   sequence int(5),
-   status varchar(20),
-   primary key (order_id, sequence)
+   order_id varchar(10) not null,
+   sequence int(5) not null,
+   status varchar(20) not null,
+   primary key (order_id, sequence),
+   foreign key (order_id) references test_order(order_id)
 );
 
-insert into test_order values ('order1', 2, 'PO');
-insert into test_order values ('order2', 33, 'PO');
-insert into test_order values ('order3', 10, 'REPLIN');
-insert into test_order values ('order4', 20, 'REPLOUT');
-insert into test_order values ('order5', 100, 'SO');
+insert into test_order (order_id, quantity, type) values ('order1', 2, 'PO');
+insert into test_order (order_id, quantity, type) values ('order2', 33, 'PO');
+insert into test_order (order_id, quantity, type) values ('order3', 10, 'REPLIN');
+insert into test_order (order_id, quantity, type) values ('order4', 20, 'REPLOUT');
+insert into test_order (order_id, quantity, type) values ('order5', 100, 'SO');
 
-insert into test_order_status values ('order1', 2, 'picked');
-insert into test_order_status values ('order1', 1, 'ordered');
-insert into test_order_status values ('order2', 3, 'out of delivery');
-insert into test_order_status values ('order2', 1, 'ordered');
-insert into test_order_status values ('order2', 2, 'shipped');
-insert into test_order_status values ('order5', 1, 'ordered');
-insert into test_order_status values ('order3', 2, 'shipped');
-insert into test_order_status values ('order3', 1, 'ordered');
-insert into test_order_status values ('order4', 4, 'delivered');
-insert into test_order_status values ('order4', 3, 'out of delivery');
-insert into test_order_status values ('order4', 1, 'ordered');
+insert into test_order_status (order_id, sequence, status) values ('order1', 2, 'picked');
+insert into test_order_status (order_id, sequence, status) values ('order1', 1, 'ordered');
+insert into test_order_status (order_id, sequence, status) values ('order2', 3, 'out of delivery');
+insert into test_order_status (order_id, sequence, status) values ('order2', 1, 'ordered');
+insert into test_order_status (order_id, sequence, status) values ('order2', 2, 'shipped');
+insert into test_order_status (order_id, sequence, status) values ('order5', 1, 'ordered');
+insert into test_order_status (order_id, sequence, status) values ('order3', 2, 'shipped');
+insert into test_order_status (order_id, sequence, status) values ('order3', 1, 'ordered');
+insert into test_order_status (order_id, sequence, status) values ('order4', 4, 'delivered');
+insert into test_order_status (order_id, sequence, status) values ('order4', 3, 'out of delivery');
+insert into test_order_status (order_id, sequence, status) values ('order4', 1, 'ordered');
+insert into test_order_status (order_id, sequence, status) values ('order4', 2, 'shipped');
+
+
+--Error
 insert into test_order_status values ('order4', 2, 'shipped');
+insert into test_order_status values ('order6', 1, 'ordered');
 
 select * from test_order;
 select * from test_order_status;
@@ -53,5 +59,5 @@ WHERE  ( status, sequence ) IN (SELECT status, sequence
                                                    FROM   test_order_status
                                                    WHERE order_id = tord.order_id) AND order_id = tord.order_id);
 
-drop table test_order;
 drop table test_order_status;
+drop table test_order;
